709e1d3369a4b2214c854017c0a59fc157a4bb0c
Javier Muraki <javimuraki@gmail.com>
2026-06-09 14:36:10 -0300

    Inicio de Ej 2

---
+++ LectorTagMP3/Id3v1Tag.cs
@@ -0,0 +1,23 @@
+namespace LectorTagMP3;
+
+public class Id3v1Tag {
+    private string? header;
+    private string? titulo;
+    private string? artista;
+    private string? album;
+    private string? anio;
+    private string? comentario;
+    private string? genero;
+
+    public string? Header       { get => header; }
+    public string? Titulo       { get => titulo; }
+    public string? Artista      { get => artista; }
+    public string? Album        { get => album; }
+    public string? Anio         { get => anio; }
+    public string? Comentario   { get => comentario; }
+    public string? Genero       { get => genero; }
+
+    public void LeerBuffer() {
+        header = "asdadwd";
+    }
+}
---
+++ LectorTagMP3/Program.cs
@@ -0,0 +1,12 @@
+﻿using LectorTagMP3;
+string? MP3_PATH; // C:\Users\Alumno\Downloads
+
+
+do { // Ingreso del MP3_PATH
+    Console.Write("Ingrese el PATH de un archivo MP3 que desea analizar: ");
+    MP3_PATH = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(MP3_PATH))    Console.WriteLine("No ingresaste nada, intenta de nuevo.\n");
+    else if (!File.Exists(MP3_PATH))       Console.WriteLine("No existe el archivo, intente de nuevo.\n");
+    else break; // Todo perfecto, se detiene el bucle
+} while (true);
